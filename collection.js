dd8daabc5e0b50d7878f08e80f7cdd3fab6d3f1f
olga0770 <olyalo0201@gmail.com>
2017-09-17 16:33:08 +0200

    Collection dynamic

---
+++ collection.js
@@ -0,0 +1,93 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+
+    console.log("siden vises");
+
+
+    $.getJSON("collection.json", visCollectionListe);
+
+}
+
+
+function visCollectionListe(listen) {
+
+    console.table( listen );
+    listen.forEach(visCollection);
+}
+
+
+
+
+function visCollection(collection) {
+    console.log( collection );
+
+
+    var klon = document.querySelector("#collection_template").content.cloneNode(true);
+
+
+    //indsæt data i klon
+    klon.querySelector(".data_collectionlink").innerHTML = collection.collectionlink;
+
+//    var element_debug = klon.querySelector(".data_arttitle");
+//    console.log("element_debug: " + element_debug);
+
+    klon.querySelector(".data_art").src = "picture/" + collection.art + "_art.jpg";
+
+/*
+    klon.querySelector(".data_arttitle").innerHTML = collection.arttitle;
+*/
+//    klon.querySelector(".data_artist").innerHTML = collection.artist;
+
+
+    document.querySelector(".picture").appendChild(klon);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
