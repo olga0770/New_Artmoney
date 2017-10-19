9b8dff8ac5a31d40fa701073be381e67969d9986
olga0770 <olyalo0201@gmail.com>
2017-10-19 22:32:43 +0200

    Filters, login, signup

---
+++ header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="picture/favicon.png">
+
+
+
+
+     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+<!--    <link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet">-->
+
+
+    <title>Artmoney</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <link href="style.css" rel="stylesheet">
+
+</head>
+
+
+<body>
+
+
+<a href="#address"><div class="logo"></div></a>
+
+<div class="shopping"></div>
+
+
+
+
+
+
+
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #828282;">
+<!--  <a class="navbar-brand" href="#">Navbar</a>-->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #000; border-radius: 0 !important">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">HOME <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="collection_wp.html">GALLERY </a>
+      </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="artists_wp.html">ARTISTS </a>
+      </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="partners.html">SHOPS </a>
+      </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="events.html">NEWS </a>
+      </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="about.html">ABOUT </a>
+      </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="faq.html">FAQ </a>
+      </li>
+
+</ul>
+
+<form class="form-inline my-2 my-lg-0">
+    <input class="form-control mr-sm-2" type="text" style="border-radius: 0 !important" placeholder="SEARCH" aria-label="Search">
+    </form>
+
+    <a href="login.html" class="btn btn-outline-light btn-square" role="button" aria-pressed="true" style="background-color: #000;">LOG IN</a>
+
+    <a href="signup.html" class="btn btn-outline-light btn-square" role="button" aria-pressed="true" style="background-color: #780a23;">JOIN US</a>
+
+</div>
+</nav>
+
+
+<p class="currency">Alternative currency made of original art</p>
